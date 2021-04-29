5437d51b8bd1eec8cc95fcdd841eab051ce4a42d
tommylu780 <nhoksun340@gmail.com>
2021-04-29 19:58:51 +1000

    add questions

--- index.js
+++ index.js
@@ -15,16 +15,48 @@ const questions = [{
         name: "description",
         message: "Write a brief description of your project: "
     },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process: "
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide project usage: "
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please provide project license: ",
+        choices: [
+            "MIT",
+            "Mozilla",
+            "Open",
+            "Academic"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who are the contributors for this project: "
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Is tests included for this project?: "
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your Github username: "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please provide your email: "
+    }
 ];
-
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-//     fileName = 'README.md';
-
-//     fs.writeFile(fileName, data, (err) =>
-//         err ? console.log(err) : console.log('Successfully created README.md file')
-//     );
-// }
 
 // TODO: Create a function to initialize app
 function init() {
