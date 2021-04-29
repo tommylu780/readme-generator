58e0288499ad3c5b3e22360d72d932fea039ccee
tommylu780 <nhoksun340@gmail.com>
2021-04-29 14:30:59 +1000

    create readme file, install inquirer

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1 align="center">asd 👋</h1>
+
--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const readmeGenerate = require("./utils/generateMarkdown.js");
+const writeDataAsync = util.promisify(fs.write);
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+        type: "input",
+        name: "title",
+        message: "What is the project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: "
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//     fileName = 'README.md';
+
+//     fs.writeFile(fileName, data, (err) =>
+//         err ? console.log(err) : console.log('Successfully created README.md file')
+//     );
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(function(data) {
+            fs.writeFile("README.md", readmeGenerate(data), (err) =>
+                err ? console.log(err) : console.log('Successfully created README.md file')
+            );
+        })
+}
 
 // Function call to initialize app
 init();
